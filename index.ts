6b592cb6ff19b0cf398fe9e9d427f572d5e5dce6
woobuntu <woobuntu2022@gmail.com>
2024-11-14 10:40:17 +0900

    비즈니스 로직 설계

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-export interface IsSpamParameters {
+export interface IsSpamParams {
   content: string;
   spamLinkDomains: string[];
   redirectDepth: number;
 }
 
-export const isSpam = (params: IsSpamParameters): Promise<boolean> => {};
+export const isSpam = (params: IsSpamParams): Promise<boolean> => {
+  // 1. content에서 url 식별
+  // 2. 해당 url이 spamLinkDomain인지 확인
+  // 3. spamLinkDomain이 아니라면 요청하여 리다이렉션 여부 확인
+  // 4. 리다이렉션된 페이지가 spamLinkDomain인지 확인
+  // 5. 라디이렉션된 페이지가 spamLinkDomain을 포함하고 있는지 확인
+  // 3~5까지는 리다이렉션 뎁스에 대해 재귀 적용 필요
+};
