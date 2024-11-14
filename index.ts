0346cd79eb4921a86195aa2a186dca6b2f53c776
woobuntu <woobuntu2022@gmail.com>
2024-11-14 10:57:47 +0900

    추출한 url 중 spam인 url이 있는지 확인

--- index.ts
+++ index.ts
@@ -17,6 +17,12 @@ export const isSpam = (params: IsSpamParams): Promise<boolean> => {
   // content 자체에 url이 없으면 스팸이 아니다.
   if (urls.length == 0) return new Promise((resolve) => resolve(false));
 
+  // 2. 해당 url이 spamLinkDomain인지 확인
+  for (const url of urls) {
+    if (params.spamLinkDomains.includes(url))
+      return new Promise((resolve) => resolve(true));
+  }
+
   // 3. spamLinkDomain이 아니라면 요청하여 리다이렉션 여부 확인
   // 4. 리다이렉션된 페이지가 spamLinkDomain인지 확인
   // 5. 라디이렉션된 페이지가 spamLinkDomain을 포함하고 있는지 확인
